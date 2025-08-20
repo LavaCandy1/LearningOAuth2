0f1ccd59cc6e689116010fdbc5f116f01d61d004
LavaCandy1 <aayush.garg.1793@gmail.com>
2025-08-20 20:42:56 +0530

    OAuth done with Google (removed API id and secret)

---
+++ src/main/java/com/LavaCandy/LearningOAuth2/HomeControler.java
@@ -0,0 +1,24 @@
+package com.LavaCandy.LearningOAuth2;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class HomeControler {
+
+    @GetMapping
+    public String home() {
+        org.springframework.security.oauth2.core.oidc.user.OidcUser oidcUser =
+            (org.springframework.security.oauth2.core.oidc.user.OidcUser)
+            org.springframework.security.core.context.SecurityContextHolder.getContext()
+            .getAuthentication().getPrincipal();
+
+        String username = oidcUser.getFullName();
+        String email = oidcUser.getEmail();
+
+        return "Welcome, " + username + "! Your email is: " + email;
+    }
+    
+}
---
+++ src/test/java/com/LavaCandy/LearningOAuth2/LearningOAuth2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.LavaCandy.LearningOAuth2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningOAuth2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
